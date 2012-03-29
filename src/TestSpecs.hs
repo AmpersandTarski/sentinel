41e8753e8363680073c43489e61e5afe63babcd0
Martijn Schrage <martijn@oblomov.com>
2012-03-29 16:19:18 +0000

    added some tests for prototype
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/Sentinel/trunk@1950 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/TestSpecs.hs
+++ src/TestSpecs.hs
@@ -10,6 +10,16 @@ testSpecs =
       , "Prototype/apps/Tests/ShouldSucceed"
       , "Prototype/apps/INDOORS/INDOORS.adl"
       ]
+  , TestSpec Prototype ["--validate"] ShouldSucceed 
+      [ "Prototype/apps/Simple"
+      , "Prototype/apps/Misc"
+      , "Prototype/apps/Tests/ShouldSucceed"
+      , "Prototype/apps/INDOORS/INDOORS.adl"
+      , "Prototype/apps/Bugs/SQL/fixed"
+      ]
+  , TestSpec Ampersand [] ShouldFail
+      [ "Prototype/apps/Tests/ShouldFail" 
+      ]
   , TestSpec Ampersand [] ShouldFail
       [ "Prototype/apps/Tests/ShouldFail" 
       ]
