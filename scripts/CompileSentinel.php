e72d29890c2b3a73453de21dde42e67b0aa86a5d
Martijn Schrage <martijn@oblomov.com>
2012-03-30 19:59:45 +0000

    couple of fixes
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/Sentinel/trunk@1982 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- scripts/CompileSentinel.php
+++ scripts/CompileSentinel.php
@@ -1,4 +1,2 @@
 <?php
 system('sudo /home/martijn/svn/Sentinel/scripts/phpRootCompileSentinel',$exitCode);
-echo '<br><br/>Compilation '.$exitCode ? 'successful' : "failed with exit code $exitCode";
-?>
--- scripts/RunSentinel.php
+++ scripts/RunSentinel.php
@@ -1,5 +1,4 @@
 <?php
 system('sudo /home/martijn/svn/Sentinel/scripts/phpRootRunSentinel',$exitCode);
-echo '<br/>Exit code'.$exitCode;
 echo '<br/><a href="SentinelOutput.txt" >View test output</a>';
 ?>
