196e3da0635688cf011dcf0b8a435de795c03826
Martijn Schrage <martijn@oblomov.com>
2012-03-31 14:37:54 +0000

    different location for RunSentinel.php, to allow for symbolic link (which survives svn update)
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/Sentinel/trunk@1985 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- scripts/RunSentinel.php
+++ scripts/RunSentinel.php
@@ -1,4 +1,4 @@
 <?php
 system('sudo /home/martijn/svn/Sentinel/scripts/phpRootRunSentinel',$exitCode);
-echo '<br/><a href="SentinelOutput.txt" >View test output</a>';
+echo '<br/><a href="ampersand/SentinelOutput.txt" >View test output</a>';
 ?>
