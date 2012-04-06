c1ddea11fcc4b162a5e119fcac79c0f0461223aa
Martijn Schrage <martijn@oblomov.com>
2012-04-06 12:14:56 +0000

    adapted to new directory structure
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/Sentinel/trunk@2009 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- www/RunSentinel.php
+++ www/RunSentinel.php
@@ -1,5 +1,5 @@
 <?php
 exec('sudo /home/martijn/svn/Sentinel/scripts/phpRootRunSentinel',$output,$exitCode);
 echo implode("<br/>", $output);
-echo '<br/><a href="ampersand/SentinelOutput.txt" >View test output</a>';
+echo '<br/><a href="../ampersand/SentinelOutput.txt" >View test output</a>';
 ?>
