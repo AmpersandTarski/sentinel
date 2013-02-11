7ffc838196bd2cd6598e76c3d3d9db0fda24d116
Martijn Schrage <martijn@oblomov.com>
2013-02-11 16:44:27 +0000

    Set timeout to 5 minutes.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/Sentinel/trunk@2290 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Execute.hs
+++ src/Execute.hs
@@ -11,7 +11,7 @@ import Utils
 import Types
 
 maxTimeInSeconds :: Int
-maxTimeInSeconds = 10
+maxTimeInSeconds = 5 * 60
 
 timeoutExitCode :: Int
 timeoutExitCode = -1 -- -1 is not used by unix processes
