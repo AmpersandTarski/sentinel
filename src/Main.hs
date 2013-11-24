5bee4183047aa8f801b83319ac4de5ed451b717e
Sebastiaan Joosten <sjcjoosten@gmail.com>
2013-11-24 12:27:44 +0000

    didn't break.. still trying to get the -xc flag working
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/Sentinel/trunk@2758 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Main.hs
+++ src/Main.hs
@@ -95,8 +95,8 @@ performTests opts =
             ; cabalUpdate
             ; putStrLn "Cleaning Ampersand"
             ; cabalClean "Ampersand" []
-            ; t2 <- reportTestResult opts $ testInstall "Ampersand" ["-f-executable","--enable-executable-profiling","--ghc-option=-fprof-auto"] "the Ampersand library"
-            ; t1 <- reportTestResult opts $ testInstall "Ampersand" ["--enable-executable-profiling","--ghc-option=-fprof-auto"]                 "the Ampersand executable (and library)" -- cannot build exec without lib because of in-place dependency
+            ; t2 <- reportTestResult opts $ testInstall "Ampersand" ["-f-executable","-p","--enable-executable-profiling","--ghc-option=-fprof-auto"] "the Ampersand library"
+            ; t1 <- reportTestResult opts $ testInstall "Ampersand" ["-p","--enable-executable-profiling","--ghc-option=-fprof-auto"]                 "the Ampersand executable (and library)" -- cannot build exec without lib because of in-place dependency
             ; putStrLn "Cleaning Prototype"
             ; cabalClean "Prototype" []
             ; t3 <- reportTestResult opts $ testInstall "Prototype" ["-p","--enable-executable-profiling","--ghc-option=-fprof-auto"] "the prototype generator"
