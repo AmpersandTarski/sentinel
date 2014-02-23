51085f160982f70992697747345f21040afcf550
Han Joosten <han.joosten.han@gmail.com>
2014-02-23 22:00:55 +0000

    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/Sentinel/trunk@2913 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Main.hs
+++ src/Main.hs
@@ -63,7 +63,7 @@ main = runCommand $ \opts _ ->
  do { initialize
     ; isTestSrv <- isTestServer
     ; (nrOfFailed, totalNrOfTests, mFailureMessage) <- performTests opts
-    ; when (optMail opts && False) $
+    ; when (optMail opts) $
         case mFailureMessage of
           Nothing -> return ()
           Just failureMessage -> when isTestSrv $
