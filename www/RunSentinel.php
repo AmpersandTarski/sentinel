a5e570e7539b417084a23fd9ea64362c5126f920
Martijn Schrage <martijn@oblomov.com>
2014-11-20 12:24:06 +0100

    Rename phpRootRunSentinel ~> phpKillAndRunSentinel

--- www/RunSentinel.php
+++ www/RunSentinel.php
@@ -1,3 +1,3 @@
 <?php
-exec('sudo -u sentinel /home/sentinel/git/sentinel/scripts/phpRootRunSentinel '. $_REQUEST['args'] .' > /dev/null &');
+exec('sudo -u sentinel /home/sentinel/git/sentinel/scripts/phpKillAndRunSentinel '. $_REQUEST['args'] .' > /dev/null &');
 ?>
