6125559338801ca3fedee73a1416ea776abaa667
Oblosys <martijn@oblomov.com>
2014-12-17 13:07:30 +0100

    Show version info for uploaded executable

--- scripts/win-publish-exe.sh
+++ scripts/win-publish-exe.sh
@@ -7,6 +7,7 @@ fi
 
 echo "Uploading $1.exe and its version info (in $1-version.txt from)"
 echo cabal bin to sentinel.tarski.nl/windowsExecutables/beta
+echo
 
 # scp doesn't like colons in $APPDATA, so first copy everything to a temp directory
 tempDirName=/tmp/sentinel-publish
@@ -17,6 +18,8 @@ rm $tempDirName/* 2> /dev/null
 
 cp $APPDATA/cabal/bin/$1.exe $tempDirName/
 $1 --version > $tempDirName/$1-version.txt
+
+cat $tempDirName/$1-version.txt
 scp $tempDirName/* sentinel@sentinel.tarski.nl:git/sentinel/www/windowsExecutables/beta/
 
 # Let's not rm -rf on variables
